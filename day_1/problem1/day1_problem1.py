e60170b89cdbcd7e1619feaab33792b546197a83
PantelisGavalas <pantelis.g7@gmail.com>
2024-12-03 14:01:55 +0100

    Initial commit: Advent of Code 2024 solutions.

---
+++ day_1/problem1/day1_problem1.py
@@ -0,0 +1,30 @@
+list1 = []
+list2 = []
+
+#with open("C:\\Users\\gavalas\\Desktop\\Advent_of_Code_2024\\day1_problem1_input.txt", "r") as file:
+with open("./day1_problem1_input.txt", "r") as file:
+
+    for line in file:
+        num1, num2 = map(int, line.strip().split())
+        list1.append(num1)
+        list2.append(num2)
+
+list1_sorted = sorted(list1)
+list2_sorted = sorted(list2)
+'''
+print(list1)
+print("\n\n\n")
+print(list1_sorted)
+print("\n\n\n\n\n\n")
+print(list2)
+print("\n\n\n")
+print(list2_sorted)
+print("\n\n\n\n\n\n")
+'''
+
+count = 0
+
+for i in range(0, len(list1_sorted)):
+    count += abs(list1_sorted[i] - list2_sorted[i])
+
+print(count)
---
+++ day_1/problem2/day1_problem2.py
@@ -0,0 +1,22 @@
+list1 = []
+list2 = []
+
+#with open("C:\\Users\\gavalas\\Desktop\\Advent_of_Code_2024\\day1_problem2_input.txt", "r") as file:
+with open("./day1_problem2_input.txt", "r") as file:
+    
+    for line in file:
+        num1, num2 = map(int, line.strip().split())
+        list1.append(num1)
+        list2.append(num2)
+
+similarity = 0
+
+for num1 in list1:
+    appearances = 0
+    for num2 in list2:
+        if num2 == num1:
+            appearances += 1
+
+    similarity += num1 * appearances
+
+print(similarity)
